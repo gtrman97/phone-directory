e56014f1e33f0daa76afbc4e62a95e20f1883065
gtrman97 <andrew.killian@g.austincc.edu>
2021-10-14 13:47:59 -0500

    added insert table row

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 const mobileRegex = /^([0-9]){10}/; 
+
+document.getElementById("submit").addEventListener("click", function() {
+
+    let name = document.getElementById("name").value; 
+    let phone = document.getElementById("phone").value; 
+    let email = document.getElementById("email").value; 
+
+    let table = document.getElementById("directory");
+    let row = table.insertRow(-1);
+    let newName = row.insertCell(0);
+    let newPhone = row.insertCell(1);
+    let newEmail = row.insertCell(2);
+
+    newName.innerText = name; 
+    newPhone.innerText = phone; 
+    newEmail.innerText = email; 
+
+
+})
