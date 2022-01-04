7e5951d81fb29a785ee7dff104f7c18371eccf9a
gtrman97 <andrew.killian@g.austincc.edu>
2022-01-04 13:18:23 -0600

    added files

---
+++ script.js
@@ -0,0 +1,8 @@
+demo.addEventListener('click', function(){
+    document.querySelector("main").style.display = "none";
+    document.getElementById("guest-signin").style.display = "flex";
+
+})
+avatar.addEventListener('click', function(){
+    
+})
