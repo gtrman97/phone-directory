1907114c4bd1ce8e426bf0f73c62a014e1f97540
gtrman97 <andrew.killian@g.austincc.edu>
2022-01-05 17:48:32 -0600

    added form

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 demo.addEventListener('click', function(){
+    
     document.querySelector("main").style.display = "none";
     document.getElementById("guest-signin").style.display = "flex";
 
 })
 create.addEventListener('click', function(){
+    
     document.querySelector("main").style.display = "none";
-    document.getElementById("guest-signin").style.display = "none";
     document.getElementById("create-account-form").style.display = "flex";
-
 
 })
